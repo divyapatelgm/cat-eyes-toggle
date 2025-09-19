9d9d7ea64d28096d895fe7e551084c2806abd85c
Divya Patel G M <divyapatelgm220604@gmail.com>
2025-09-19 19:34:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function switchOff(){
-    document.getElementById("bulbImage").src = "assets/bulb_on.png"; //changes to bulb on when switched off
-    document.getElementById("catImage").src = "assets/cat_eyes.png";
+    document.getElementById("bulbImage").src = "https://res.cloudinary.com/dd8ipwito/image/upload/v1758290227/bulb_off_saqlz6.png"; //changes to bulb on when switched off
+    document.getElementById("catImage").src = "https://res.cloudinary.com/dd8ipwito/image/upload/v1758290538/cat_eyes_m0rdj9.png";
     document.getElementById("switchStatus").textContent = "Switched Off";
     document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9";
     document.getElementById("offSwitch").style.backgroundColor = "#c53822ff"; // red
 }
 function switchOn(){
-    document.getElementById("bulbImage").src = "assets/bulb_on.png"; //changes to bulb on when switched on
-    document.getElementById("catImage").src = "assets/cat.png";
+    document.getElementById("bulbImage").src = "https://res.cloudinary.com/dd8ipwito/image/upload/v1758290540/bulb_on_f7vetx.png"; //changes to bulb on when switched on
+    document.getElementById("catImage").src = "https://res.cloudinary.com/dd8ipwito/image/upload/v1758290539/cat_ydgn9m.png";
     document.getElementById("switchStatus").textContent= "Switched On";
     document.getElementById("onSwitch").style.backgroundColor = "#22c55e"; // green
     document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9";
